b383293a585aa3f4b3e809c2d449997003d5806b
inseong choi <masil20203030@gmail.com>
2026-03-14 19:13:37 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace CatchButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.BackColor = System.Drawing.Color.Cyan;
+            this.run.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.run.ForeColor = System.Drawing.Color.Navy;
+            this.run.Location = new System.Drawing.Point(100, 150);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(170, 100);
+            this.run.TabIndex = 0;
+            this.run.Text = "잡아보소";
+            this.run.UseVisualStyleBackColor = false;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            this.run.MouseEnter += new System.EventHandler(this.run_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.run);
+            this.Name = "Form1";
+            this.Text = "Catch the button version 1.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchButton
+{
+    public partial class Form1 : Form
+    {
+        // 필드 선언
+        private Random rd;
+        private int x;
+        private double randomDouble;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 초기화 시점에 Random 객체를 생성하고 난수를 생성합니다.
+            rd = new Random(); // 랜덤객체생성
+            x = rd.Next(0, 500);  // 0 이상500 미만의정수반환
+            randomDouble = rd.NextDouble(); // 0.0과1.0 사이의실수반환
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            // 새로운위치값세팅. 새로운X,Y 좌표값
+            int x_position = 100;
+            int y_position = 150;
+
+            // 버튼을 새로운 위치로 옮김(새로운 Point 객체 생성)
+            run.Location = new Point(x_position, y_position);
+
+        }
+
+        private void run_MouseEnter(object sender, EventArgs e)
+        {
+
+            // 1. 난수 생성기 준비
+            Random localRd = new Random();
+
+            // 2. 가용 영역 계산(버튼이 폼 테두리에 걸리지 않게 보호)
+            // ClientSize는 타이틀바와 테두리를 제외한 실제 그리기 영역입니다.
+            int maxX = this.ClientSize.Width - run.Width;
+            int maxY = this.ClientSize.Height - run.Height;
+            if (maxX < 0) maxX = 650;
+            if (maxY < 0) maxY = 400;
+
+            // 3. 랜덤 좌표 추출(0 ~ 최대 가용치 사이)
+            int nextX = localRd.Next(0, maxX + 1);
+            int nextY = localRd.Next(0, maxY + 1);
+
+            // 4. 위치 할당(새로운 Point 객체 생성)
+            run.Location = new Point(nextX, nextY);
+
+            // 5. 시각적 피드백(폼 제목 표시줄에 좌표 출력)
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
+
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
